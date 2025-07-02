be3142443d4e50a6785647afaa53c3632ba6fdab
ViniCarmo <vinicius.carmo1106@gmail.com>
2025-07-02 20:00:17 -0300

    Create entity autor

---
+++ src/main/java/com/cursospring/libraryapi/model/Autor.java
@@ -0,0 +1,32 @@
+package com.cursospring.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "autor")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Autor {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", length = 100, nullable = false)
+    private String nome;
+
+    @Column(name = "ndata_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade", length = 50, nullable = false)
+    private String nacionalidade;
+
+}
