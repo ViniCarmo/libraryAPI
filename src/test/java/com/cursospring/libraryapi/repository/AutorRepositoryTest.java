fba1bfd3b4a424d06d7e5c8de6940418c42ab497
ViniCarmo <vinicius.carmo1106@gmail.com>
2025-07-08 14:46:56 -0300

    Terminando testAutor

--- src/test/java/com/cursospring/libraryapi/repository/AutorRepositoryTest.java
+++ src/test/java/com/cursospring/libraryapi/repository/AutorRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -32,16 +33,40 @@ public class AutorRepositoryTest {
         var id = UUID.fromString("2c527e4e-f768-4134-9f24-b35ba410f13e");
         Optional<Autor> possivelAutor = autorRepository.findById(id);
 
-        if (possivelAutor.isPresent()){
+        if (possivelAutor.isPresent()) {
 
             Autor autorEncontrado = possivelAutor.get();
-        System.out.println("Dados do autor: ");
-        System.out.println(autorEncontrado);
+            System.out.println("Dados do autor: ");
+            System.out.println(autorEncontrado);
 
-        autorEncontrado.setDataNascimento(LocalDate.of(1980, 1, 31));
+            autorEncontrado.setDataNascimento(LocalDate.of(1980, 1, 31));
 
-        autorRepository.save(autorEncontrado);
+            autorRepository.save(autorEncontrado);
 
+        }
     }
-}
+
+    @Test
+    public void listarTest() {
+        List<Autor> lista = autorRepository.findAll();
+        lista.forEach(System.out::println);
+    }
+
+    @Test
+    public void countTest(){
+        System.out.println("Contagem de autores: "  + autorRepository.count());
+    }
+
+    @Test
+    public void deleteIDTest(){
+        var id = UUID.fromString("2c527e4e-f768-4134-9f24-b35ba410f13e");
+        autorRepository.deleteById(id);
+    }
+
+    @Test
+    public void deleteTest(){
+        var id = UUID.fromString("d1c189fb-a4c7-4340-ac2f-b04bd0b9f974");
+        var maria = autorRepository.findById(id).get();
+        autorRepository.delete(maria);
+    }
 }
